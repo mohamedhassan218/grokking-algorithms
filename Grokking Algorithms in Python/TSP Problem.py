42bbd07383f8b0cec4b6a328a5c33f09ade585b8
mohamedhassan218 <m.hassan.def@gmail.com>
2023-05-18 20:17:46 +0300

    Begin the code in TSP

---
+++ Grokking Algorithms in Python/TSP Problem.py
@@ -0,0 +1,109 @@
+from operator import attrgetter
+import random, sys, time, copy
+
+"""
+    Class that implement a generic graph, it creates empty dectionaries to store the edges and 
+    empty set to store the vertices. It enables you to add edge between src and destination vertices.
+    It can calculate the total path of the graph, generate random pathes in your graph and show it with 
+    all vertices and the weight between them. 
+"""
+class Graph():
+    def __init__(self, amount_vertices):
+        self.edges = {}
+        self.vertices = set()
+        self.amount_vertices = amount_vertices
+
+    def add_edge(self, src, dest, cost = 0):
+        if not self.exists_edge(src, dest):
+            self.edges[(src, dest)] = cost
+            self.vertices.add(src)
+            self.vertices.add(dest)
+
+    def exists_edge(self, src, dest):
+        return (True if (src, dest) in self.edges else False)    
+    
+    def show_graph(self):
+        print("I'll show the graph now:\n")
+        for edge in self.edges:
+            print("Node %d linked with node %d during a cost of %d" % (edge[0], edge[1], self.edges[edge]))
+
+    def get_cost_path(self, path):
+        total_cost = 0
+        for i in range(self.amount_vertices - 1):
+            total_cost += self.edges[(path[i], path[i+1])]
+            total_cost += self.edges[(path[self.amount_vertices - 1], path[0])]
+            return total_cost
+    
+    def get_random_pathes(self, max_size):
+        random_paths, list_vertices = [], list(self.vertices)
+        initial_vertice = random.choice(list_vertices)
+        if initial_vertice not in list_vertices:
+            print("Error, initial vertice %d not exists." % initial_vertice)
+            sys.exit(1)
+        list_vertices.remove(initial_vertice)
+        list_vertices.insert(0, initial_vertice)
+        for i in range(max_size):
+            temp_list = list_vertices[1:]
+            random.shuffle(temp_list)
+            temp_list.insert(0, initial_vertice)
+            if temp_list not in random_paths:
+                random_paths.append(temp_list)
+        return random_paths
+
+"""
+    A subclass that inherits form the generic graph class and adds a method
+    specifically for generatin a complete graph.
+"""
+class Complete_Graph(Graph):
+    def generate(self):
+        for i in range(self.amount_vertices):
+            for j in range(self.amount_vertices):
+                if i != j:
+                    weight = random.randint(1, 10)
+                    self.add_edge(i, j, weight)
+
+"""
+    This class represents a particle in a particle swarm optimization (PSO) algorithm. Each particle
+    has a solution, personal best (pbest), current cost of the solution, velocity and methods to access
+    these attributes.
+"""
+class Particle:
+    def __init__(self, solution, cost):
+        self.solution = solution
+        self.pbest = solution
+        self.cost_current_solution = cost
+        self.cost_pbest_solution = cost
+        self.velocity = []
+    
+    def set_pbest(self, new_pbest):
+        self.pbest = new_pbest
+
+    def get_pbest(self):
+        return self.pbest
+    
+    def set_velocity(self, new_velocity):
+        self.velocity = new_velocity
+    
+    def get_velocity(self):
+        return self.velocity
+    
+    def set_current_solution(self, solution):
+        self.solution = solution
+    
+    def get_current_solution(self):
+        return self.solution
+    
+    def set_cost_pbest(self, cost):
+        self.cost_pbest_solution = cost
+    
+    def get_cost_pbest(self):
+        return self.cost_pbest_solution
+    
+    def set_cost_current_solution(self, cost):
+        self.cost_current_solution = cost
+
+    def get_cost_current_solution(self):
+        return self.cost_current_solution
+    
+    def clear_velocity(self):
+        del self.velocity[:]
